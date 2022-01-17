0cf8562d1fba94be1e1a43df31b82419d217edd7
hyeonah <hyeona0000@naver.com>
2022-01-17 16:32:50 +0900

    First

---
+++ app/src/main/java/com/example/runwith/FCM/FirebaseInstanceIDService.java
@@ -0,0 +1,6 @@
+package com.example.runwith.FCM;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class FirebaseInstanceIDService extends FirebaseInstanceIdService{
+}
